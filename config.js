90a915ce02efc7509090c1c3209ca3cfb230230e
krak <notreallykrak@gmail.com>
2025-06-10 22:49:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-{
-    "token": "MTM3NDc3MzM4NTA1NzQwNjk3Ng.G7PhxI.MysO8Sh7MiMOV4tc8yxD9t6rKjmBq3CsES5Aes",
-    "log": "1374772399689437237",
-    "sahip": "999406459106373722",
-    "desteksunucusu": "https://discord.gg/QUjaT4RzCK",
-    "bot-davet": "https://discord.com/oauth2/authorize?client_id=1374773385057406976&permissions=8&integration_type=0&scope=bot+applications.commands",
-    "bot-adi": "Nuron's Krak",
-    "clientId": "1374773385057406976",
-    "mesajXp": 1,
-    "sesXp": 2,
-    "levelXp": 99,
-    "embedColor": "#5865F2",
-    "errorColor": "#ED4245",
-    "successColor": "#57F287",
-    "footer": "💨 reallykrak"
-  }
+export const config = {
+  token: "MTM3NDc3MzM4NTA1NzQwNjk3Ng.G7PhxI.MysO8Sh7MiMOV4tc8yxD9t6rKjmBq3CsES5Aes",
+  log: "1374772399689437237",
+  sahip: "999406459106373722",
+  desteksunucusu: "https://discord.gg/QUjaT4RzCK",
+  "bot-davet": "https://discord.com/oauth2/authorize?client_id=1374773385057406976&permissions=8&integration_type=0&scope=bot+applications.commands",
+  "bot-adi": "Nuron's Krak",
+  clientId: "1374773385057406976",
+  mesajXp: 1,
+  sesXp: 2,
+  levelXp: 99,
+  embedColor: "#5865F2",
+  errorColor: "#ED4245",
+  successColor: "#57F287",
+  footer: "💨 reallykrak"
+};
