727b22968a28e99f549b3d9bb453841a8375f81c
krak <notreallykrak@gmail.com>
2025-06-10 22:55:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-export const config = {
-  token: "MTM3NDc3MzM4NTA1NzQwNjk3Ng.G7PhxI.MysO8Sh7MiMOV4tc8yxD9t6rKjmBq3CsES5Aes",
-  log: "1374772399689437237",
-  sahip: "999406459106373722",
-  desteksunucusu: "https://discord.gg/QUjaT4RzCK",
-  "bot-davet": "https://discord.com/oauth2/authorize?client_id=1374773385057406976&permissions=8&integration_type=0&scope=bot+applications.commands",
-  "bot-adi": "Nuron's Krak",
-  clientId: "1374773385057406976",
-  mesajXp: 1,
-  sesXp: 2,
-  levelXp: 99,
-  embedColor: "#5865F2",
-  errorColor: "#ED4245",
-  successColor: "#57F287",
-  footer: "💨 reallykrak"
-};
+{
+    "token": "37737337379292TRHRH",
+    "log": "99999999775",
+    "sahip": "seninid",
+    "desteksunucusu": "https://discord.gg/vorrndc",
+    "bot-davet": "sleam",
+    "bot-adi": "Tolu",
+    "clientId": "8838383",
+    "mesajXp": 1,
+    "sesXp": 2,
+    "levelXp": 99,
+    "embedColor": "#5865F2",
+    "errorColor": "#ED4245",
+    "successColor": "#57F287",
+    "footer": "📊 discord.gg/trc"
+}
