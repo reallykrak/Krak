e9ce99f249b8530ad28d2c50d4df170910c07414
krak <notreallykrak@gmail.com>
2025-06-10 21:56:07 +0300

    Create logger.js

---
+++ utils/logger.js
@@ -0,0 +1,26 @@
+import chalk from 'chalk';
+
+const logLevels = {
+  info: chalk.blue,
+  warn: chalk.yellow,
+  error: chalk.red,
+  debug: chalk.green,
+};
+
+const createLogger = () => {
+  const logger = {};
+
+  Object.keys(logLevels).forEach(level => {
+    logger[level] = (message) => {
+      const colorize = logLevels[level];
+      const timestamp = new Date().toISOString();
+      console.log(colorize(`[${timestamp}] [${level.toUpperCase()}]: ${message}`));
+    };
+  });
+
+  return logger;
+};
+
+globalThis.log = createLogger();
+
+export default log;
